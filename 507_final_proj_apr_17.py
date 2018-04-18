efd7039ef2dcb1a3847dc1c7eaf8b9c59a639abb
Jordan A. Earnest <jordanearnest1@gmail.com>
2018-04-18 12:18:47 -0400

    commit

--- 507_final_proj_apr_17.py
+++ 507_final_proj_apr_17.py
@@ -7,7 +7,8 @@ import requests.auth
 from datetime import *
 from reddit_secrets import *
 import os
-
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 CLIENT_ID = client_id
 CLIENT_SECRET = client_secret
@@ -43,13 +44,13 @@ def create_reddit_db():
         "Pinned_content" TEXT,
         "Original_Content" TEXT,
         "Contains_video" TEXT
-        "Number_Upvotes" INTEGER NOT NULL,
+        "Number_Upvotes" INTEGER NOT NULL
         )'''
         # "Post_Title" TEXT,        
         # "Post_Date" TEXT,
         # "Contains_link" NUMERIC NOT NULL
     cur.execute(create_table_one)
-    conn.commit
+    conn.commit()
 
     statement = '''
         DROP TABLE IF EXISTS "PostVotes";
@@ -105,11 +106,14 @@ def load_cache():
     global CACHE_FNAME
     check_cache = check_cache_time()
     if check_cache is False:
+        print("CHECK CACHE IN LOAD CACHE IS FALSE")
         with open(CACHE_FNAME, "r") as cache_file:
             cache_string = cache_file.read()
             CACHE_DICTION = json.loads(cache_string)
         return CACHE_DICTION
-
+    else:
+        CACHE_DICTION = {}
+        return CACHE_DICTION
 
 
 #  Saves cache contents into a json file
@@ -119,6 +123,7 @@ def write_cache_data(data_to_cache):
     cache_string = json.dumps(data_to_cache)
     cache_file.write(cache_string)
     cache_file.close()
+
 
 def get_reddit_creds():
     headers = {"Authorization": "bearer fhTdafZI-0ClEzzYORfBSCR7x3M", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
@@ -136,32 +141,24 @@ def make_reddit_request():  ## ---> if cache dict is empty, make new request
     headers = {"Authorization": "bearer " + creds["access_token"], "User-Agent": "subreddit top scores script by /u/" + username}
     params = {}
     response2 = requests.get("https://oauth.reddit.com/" + "top", headers=headers, params = {'sort': 'top','limit': 30})
-    # for i in json.loads(response2.text):
-    #     print(i)
-    response_text = json.loads(response2.text)
-    # print("response_text")
-    # print(response_text)
-    cache_file = open(CACHE_FNAME,"w")
-    cache_string = json.dumps(response_text)
-    print("===================")
-    print("THIS IS MY CACHE STRING AND TYPE")
-    # print(cache_string)
-    # print(type(cache_string))
-    cache_file.write(cache_string)
-    print("GOT HERE")
-    cache_file.close()
-    return response_text
 
+    response_text = json.loads(response2.text)
+    write_cache_data(response_text)
+    print("this is the type of the response")
+    print(type(response_text))
+    # cache_file = open(CACHE_FNAME,"w")
+    # cache_string = json.dumps(response_text)
+    # cache_file.write(cache_string)
+    # cache_file.close()
+    return response_text
 
 
 # loaded_cache = load_cache()   ## returns a CACHE_Diction either way ##--> this is something to do when start the program 
 # ##if the time stamp is too old, then 
 
-# make = make_reddit_request(loaded_cache)
 
 
-
-def populate_reddit_db(reddit_request):   ## will need to read it from a cache file either way. 
+def populate_reddit_db(cache_dict):   ## will need to read it from a cache file either way. 
     print(CACHE_DICTION)
     conn = sqlite.connect(DBNAME)
     cur = conn.cursor()
@@ -181,87 +178,58 @@ def populate_reddit_db(reddit_request):   ## will need to read it from a cache f
         Number_Upvotes = result["ups"]
 
         insert_statement = '''
-            INSERT INTO PostContent(Subreddit_Id, Subreddit_Name, Post_Id, subreddit_subscribers, Thread_Title, Pinned_content, Original_Content, Contains_video) VALUES (?,?,?,?,?,?,?,?)
+            INSERT INTO PostContent(Subreddit_Id, Subreddit_Name, Post_Id, subreddit_subscribers, Thread_Title, Pinned_content, Original_Content, Contains_video, Number_Upvotes) VALUES (?,?,?,?,?,?,?,?,?)
             ''' 
-        cur.execute(insert_statement, [Subreddit_Id, Subreddit_Name, Post_Id, subreddit_subscribers, Thread_Title, Pinned_content, Original_Content, Contains_video])
+        cur.execute(insert_statement, [Subreddit_Id, Subreddit_Name, Post_Id, subreddit_subscribers, Thread_Title, Pinned_content, Original_Content, Contains_video, Number_Upvotes])
         conn.commit()
 
     conn.close()
 
 
+## for now while i try out getting the results
+##create_reddit_db()
 
-loaded_cache = load_cache() 
-
+loaded_cache = load_cache()   ## Returns a cache diction
+print("this is the type")
+print(type(loaded_cache))
 print(loaded_cache.keys())
 # loaded_cache = {}
 # loaded_cache = {SOME PREVIOUS DATA}
-if len(loaded_cache.keys()) > 0: 
 
-    print("--------------")
-    loaded_cache = make_reddit_request()
-
-print(loaded_cache.keys())
-#
-
-
-##just got rid of april 17
-# reddit_request = make_reddit_request(loaded_cache)
-
-
-# cache_data(reddit_request)
-# populate_reddit_db(reddit_request)
+#### will need this again later  ####
+# if len(loaded_cache.keys()) > 0: ## meaning, if i have emptied my cach
+#     print("--------------")
+#     print("the length of my cache diction keys is: ")
+#     print(len(loaded_cache.keys()))
+#     loaded_cache = make_reddit_request()
+#     print("THIS Is the type of my loaded cache")
+#     print(type(loaded_cache))
+# else:
+#     print("here, went to else")
 
 
-
-##### NOT USING!!! #####
-# class Post():
-#     def __init__(self):
-#         self.
-
-
-# link_flair_type
-# media 
-#    #(true or false)
-
-# ["ups"]
-
-
-
-
-
-    # cur.execute('''CREATE TABLE IF NOT EXISTS PostVotes(
-    #             "ID" SERIAL PRIMARY KEY,
-    #             "Post_Id" TEXT NOT NULL,
-    #             "Subcategory_Name" TEXT NOT NULL,
-    #             "Subcategory_Id" INTEGER NOT NULL,
-    #             "Number_Upvotes" INTEGER NOT NULL,
-    #             "Number_Downvotes" INTEGER NOT NULL)
-    #             ''')
+# for r in loaded_cache["data"]["children"]:
+#     rr = r["data"]["pinned"]
+#     print(rr)
+#         # print(x)
+#     print("==============")
+count = 0
+for post in loaded_cache["data"]["children"]:
+        result = post["data"]
+        print(result["subreddit"])
+        Subreddit_Id = result["subreddit_id"]
+        Subreddit_Name = result["subreddit"]
+        # Post_Title = result["selftext"]
+        Post_Id = result["id"]
+        subreddit_subscribers = result["subreddit_subscribers"]
+        Thread_Title = result["title"]
+        Pinned_content = result["pinned"]
+        Original_Content = result["is_original_content"]
+        Contains_video = result["is_video"]
+        Number_Upvotes = result["ups"]
+        count +=1
 
 
 
-
-
-
-
-
-#### NOT USING RIGHT NOW #####
-##### Setting up the database connection #####
-# try:
-#     if db_password != "":
-#         conn = psycopg2.connect("dbname = '{0}' user = '{1}' password = '{2}'".format(db_name, db_user, db_password))
-#         print("Success connecting to the database")
-#     else:
-#         conn = psycopg2.connect("dbname = '{0}' user = '{1}'".format(db_name, db_user))
-#         print("Success connecting to the database")
-
-# except:
-#     print("Unable to connect to the database")
-#     sys.exit(1)
-
-# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-##### Seting up the database #####
-# def setup_database():
 
 
